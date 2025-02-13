15fe889f4390a96e49821d773ebb3c88085f7b26
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2025-02-13 14:24:25 +0100

    feat: work on user

---
+++ src/main/java/com/example/toktik_userservice/model/Gender.java
@@ -0,0 +1,13 @@
+package com.example.toktik_userservice.model;
+
+/**
+ * Enum representing gender of a user
+ * @see User
+ */
+public enum Gender {
+    MALE,
+    FEMALE,
+    NON_BINARY,
+    OTHER,
+    PREFER_NOT_TO_SAY,
+}
--- src/main/java/com/example/toktik_userservice/model/User.java
+++ src/main/java/com/example/toktik_userservice/model/User.java
@@ -2,43 +2,93 @@ package com.example.toktik_userservice.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.util.UUID;
 
+/**
+ * A user entity that represents a user in the database.
+ * This is used to store user information in the application.
+ */
 @Entity
 @Getter
 @Setter
-@RequiredArgsConstructor
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@ToString
+@ToString(exclude = "password")
 @Table(name = "user")
 public class User {
+    /**
+     * The unique identifier of the user.
+     * This is used to identify the user in the database.
+     * This is set automatically when the user is created.
+     * @see UUID
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(nullable = false)
     private UUID id;
 
-    @NonNull
+    /**
+     * A unique username for the user.
+     * This is used for login.
+     */
+    @Column(nullable = false, unique = true)
     private String username;
 
-    @NonNull
-    private String vorname;
+    /**
+     * The first name of the user.
+     */
+    @Column(nullable = false)
+    private String firstName;
 
-    @NonNull
-    private String nachname;
+    /**
+     * The last name of the user.
+     */
+    @Column(nullable = false)
+    private String lastName;
 
-    @NonNull
+    /**
+     * The birthday of the user.
+     * This is used to calculate the age of the user.
+     * The age of the user is not stored in the database.
+     */
+    @Column(nullable = false)
     private LocalDate birthday;
 
-    @NonNull
+    /**
+     * The email address of the user.
+     * This is used for login.
+     */
+    @Column(nullable = false, unique = true)
     private String email;
 
-    @NonNull
-    private String gender;
+    /**
+     * The gender of a user.
+     * This is used to address the user correctly.
+     * @see Gender
+     */
+    @Column(nullable = false)
+    private Gender gender;
 
-    @NonNull
+    /**
+     * The password of the user.
+     * This is used for login.
+     * The password is hashed before it is stored in the database.
+     */
+    @Column(nullable = false)
+    @ToString.Exclude
     private String password;
+
+    /**
+     * Timestamp when the user was created.
+     * This is used to track when the user was created.
+     * This is set automatically when the user is created.
+     */
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
 }
